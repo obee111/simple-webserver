19bca342a14e5d9047e1910a0c3102cbec9a534d
obee111 <burnerob04@gmail.com>
2024-12-18 19:38:17 -0700

    Post still doesn't work, who cares

--- server.js
+++ server.js
@@ -4,19 +4,23 @@ var fs = require("fs");
 var config = {
 	indexFile: "index.html", // index file refers to what the main page of each dir should be named
 	contentDir: "./content/", // basically where all the junk is hiding
-	charset: "utf8"
+	charset: "utf8",
+	port: 8080,
+	host: "localhost"
 }
 
-var port = 8080;
-var host = "localhost";
-
 var server = http.createServer((req, res) => {
+	let urlVals = req.url.split("?");
+	let link = urlVals[0];
+	let vals = urlVals[1];
+	console.log(`${req.method}  ${req.url}`)
+	// console.log(`${link} and ${vals}`)
 	switch (req.method){
 		// if its a get request
 		case "GET":
 			// first determine if they want index, index for another dir, or a specific file 
 			let file
-			if (req.url == "/") {
+			if (link == "/") {
 				file = config.contentDir + config.indexFile
 				fs.readFile(file, (err, data) => {
 					if (err){
@@ -31,16 +35,16 @@ var server = http.createServer((req, res) => {
 				})
 			}
 			// if theres no file extension (no ".")
-			else if (req.url.split(".").length === 1){
+			else if (link.split(".").length === 1){
 				// first take the content directory
 				file = config.contentDir
-				if (req.url[req.url.length-1] !== "/") {
+				if (link[link.length-1] !== "/") {
 					// if theres no / at the end add it in and add the index file name
-					file += req.url + "/" + config.indexFile
+					file += link + "/" + config.indexFile
 				}
 				else {
 					// if theres a / just slap that sammy together
-					file += req.url + config.indexFile
+					file += link + config.indexFile
 				}
 				fs.readFile(file, (err, data) => {
 					// if we cant find it we tell them 
@@ -57,7 +61,7 @@ var server = http.createServer((req, res) => {
 			}
 			else {
 				// this is the weird one. If they want a file we need to get the right file and the headers
-				fs.readFile(config.contentDir + req.url, (err, data) => {
+				fs.readFile(config.contentDir + link, (err, data) => {
 					// if the file is MIA just tell them to get f***ed
 					if (err) {
 						res.writeHead(404, {"Content-Type": "text/html"})
@@ -66,7 +70,7 @@ var server = http.createServer((req, res) => {
 					// if we cant call them idiots we need to do some work
 					else {
 						// isolate the extension 
-						let ext = req.url.split(".")
+						let ext = link.split(".")
 						ext = ext[ext.length-1]
 						// placeholders for the two parts of content type header
 						let type
@@ -100,13 +104,26 @@ var server = http.createServer((req, res) => {
 			}
 			break
 		// if it isnt a get request I'm not dealing with it right now. I'm busy 
+		case "POST":
+			let body='';
+			req.on('data', (data => {
+				body += data;
+				console.log(`data: ${data}`)
+			}))
+			req.on('end', ()=>{
+				res.writeHead(200, {"Content-Type":"text/plain"})
+				res.end(body)
+			})
+			console.log(body)
+			break
 		default:
 			res.writeHead(403, {"Content-Type": "text/plain"})
 			res.end("Forbidden")
 			break
+
 	}
 })
 
-server.listen(port, host, ()=>{
+server.listen(config.port, config.host, ()=>{
 	console.log("she goin");
 })
